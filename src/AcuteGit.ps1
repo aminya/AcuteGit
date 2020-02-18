527d4482a9be3c19b334d67836b1da24fb69b9b8
aminya <aminyahyaabadi74@gmail.com>
2020-02-18 05:46:36 -0600

    simplifying the code

--- src/AcuteGit.ps1
+++ src/AcuteGit.ps1
@@ -1,24 +1,16 @@
-Function Get-Folder($initialDirectory) {
-    [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
-    $FolderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog
-    $FolderBrowserDialog.RootFolder = 'MyComputer'
-    if ($initialDirectory) { $FolderBrowserDialog.SelectedPath = $initialDirectory }
-    [void] $FolderBrowserDialog.ShowDialog()
-    return $FolderBrowserDialog.SelectedPath
-}
-# ($FolderPermissions = Get-Folder C:\Users | get-acl | select -exp access | ft)
-
-
-$button2_Click = {
-    echo "This script will update the local and remote fork"
+$update_fork_Click = {
+	echo "This script will update the local and remote fork"
+    [void] $update_fork_folderBrowser.ShowDialog()
+    
     # $path = Read-Host 'Enter path of the repository'
-    $path = Get-Folder
+    $path = $update_fork_folderBrowser.SelectedPath
 
     cd $path
     git fetch upstream
     git reset --hard upstream/master
     git push -f
 }
+
 
 . (Join-Path $PSScriptRoot 'AcuteGit.designer.ps1')
 
