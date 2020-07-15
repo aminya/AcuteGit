ace63be212209403b92d492231642622a85173a6
aminya <aminyahyaabadi74@gmail.com>
2020-07-14 22:20:55 -0500

    resources

--- src/AcuteGit.ps1
+++ src/AcuteGit.ps1
@@ -1,8 +1,26 @@
+$removespace_Click = {
+    [void] $removespace_folderBrowser.ShowDialog()
+    $path = $removespace_folderBrowser.SelectedPath
+    
+    while (IFS= read -r -d '' -u 9){
+
+    }
+    do
+    if [[ "$(file -bs --mime-type -- "$REPLY")" = text/* ]]
+    then
+        sed -i 's/[ \t]\+\(\r\?\)$/\1/' -- "$REPLY"
+    else
+        echo "Skipping $REPLY" >&2
+    fi
+    done 9< <(find . -type f -print0)
+
+}
+
 $update_fork_Click = {
 	echo "This script will update the local and remote fork"
-    [void] $update_fork_folderBrowser.ShowDialog()
-    
     # $path = Read-Host 'Enter path of the repository'
+
+    [void] $update_fork_folderBrowser.ShowDialog()
     $path = $update_fork_folderBrowser.SelectedPath
 
     cd $path
