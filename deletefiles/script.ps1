f1585fad5629435cd8bb93589b6ad284ae3554fc
Amin Yahyaaabadi <aminyahyaabadi74@gmail.com>
2020-12-31 03:54:05 -0600

    Delete unused scripts

--- deletefiles/script.ps1
+++
@@ -1,6 +0,0 @@
-$Files = "indent-detective.d.ts","selector.d.ts", "status.d.ts", "tsconfig.tsbuildinfo", "selector.js", "status.js"
- foreach ($File in $Files) {
-    java -jar bfg.jar --delete-files $File --no-blob-protection .
-	rm $File
-	git reflog expire --expire=now --all && git gc --prune=now --aggressive
-}
